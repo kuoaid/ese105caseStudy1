2b4756d49f6344277af5b5a3db2f4bf704f5d53d
Chang-Sam <changyufei2010@gmail.com>
2022-09-30 15:55:14 -0500

    <any message, like hi>

---
+++ edit_me.m
@@ -0,0 +1,4 @@
+%1. load files
+%2. run kmeans to get a column of groups (todo: design the kmeans)
+%3. concatenate group with cnty_covid or cnty_census(better)
+%note down additional work that we may need
