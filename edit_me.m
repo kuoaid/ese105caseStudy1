a6ee11da2c02609713d6b5f2fcf83ad82bc30fe9
adminKuo-at-423754649196 <awang8396@gmail.com>
2022-10-02 16:22:39 -0500

    added comments and instructions

--- edit_me.m
+++ edit_me.m
@@ -7,22 +7,32 @@ load('COVIDbyCounty.mat');
 %%
 
 
-
-% figure
-% silhouette(CNTY_COVID, idx);
-
-%%
+%this is where you process the raw data. NEVER sort this or add rows with
+%data that could be meaningless/containminating to a kmeans function.
 speed = diff(CNTY_COVID')';
 accel = [zeros(225,2) diff(speed')'];
 
-[m,n] = size(accel) ;
+%the following step does the sorting and the kmeans on the processed data.
+%if you ever change the dataset (linear transformation, etc), reassign the
+%variable processed to point to your newly processed data here:
+
+processed = accel;%change accel to your new data name.
+
+%now
+
+[m,n] = size(processed) ;
 P = 0.80 ;
-split_idx = randperm(m)  ;
-training = accel(split_idx(1:round(P*m)),:); 
-testing = accel(split_idx(round(P*m)+1:end),:);
+split_idx = randperm(m);
+training = processed(split_idx(1:round(P*m)),:); 
+
+%the testing data is concatenated with all the known data attributes to aid
+%future testing.
+data_with_characteristics = [array2table(divisionLabels) CNTY_CENSUS array2table(processed)];
+testing_data = processed(split_idx(round(P*m)+1:end),:);
+testing_table = data_with_characteristics(split_idx(round(P*m)+1:end),:);
 
 
 [trained_idx,centroids] = kmeans(training, 9, 'Replicates', 20);
 
-idx_accel = [trained_idx,training];
-sorted_accel = sortrows(idx_accel,1);
+idx_trained_data = [trained_idx,training];
+sorted_trained_data = sortrows(idx_trained_data,1);
